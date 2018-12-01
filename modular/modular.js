fa9d72c78c353a1445d0e47c29a4b13ef00e3d19
Rammoun <44053646+Rammoun@users.noreply.github.com>
2018-12-01 10:20:12 +0100

    Delete modular.js

--- modular/modular.js
+++
@@ -1,9 +0,0 @@
-const process=require("process")
-/*const fs=require("fs")
-const path = require('path');*/
-const mod=require('./module')
-
-
-var res=mod(process.argv);
-//console.log(res)
-res.map((el)=>(console.log(el)));
